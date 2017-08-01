bbccbf491bb7d497c308de03410f041759d761cb
dfan97 <davidfan219@gmail.com>
2017-08-01 00:34:31 -0400

    Rudimentary resizing fix for homepage + added bio pics for ro and cam on

--- js/countdown/main.js
+++ js/countdown/main.js
@@ -78,7 +78,7 @@
 				+ '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Day%!d</span></div>'
 				+ '<div class="counter-box"><div class="number">%H</div><span>Hours</span></div>'
 				+ '<div class="counter-box"><div class="number">%M</div><span>Minutes</span></div>'
-				+ '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'
+				// + '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'
 			));
 		});
 
