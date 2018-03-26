7fa7412440649c38f9d78c6b9b743da55659ca12
dfan97 <davidfan219@gmail.com>
2018-03-26 10:20:44 -0400

    Final?? iteration of 2018 site

--- scripts/script.R
+++ scripts/script.R
@@ -1,7 +1,7 @@
 library(ggmap)
 library(zipcode)
 setwd("/Users/dfan/Dropbox (Personal)/Programming/Projects/puscioly/scripts")
-df <- read.csv("schools2018.csv", colClasses = c("character", "double", "character", "character", "double", "logical"))
+df <- read.csv("schools2018.csv", colClasses = c("character", "double", "character", "character", "character", "double", "logical"))
 data(zipcode)
 df$Latitude <- sapply(df$Zip, function(x) {
   zipcode[which(zipcode$zip == x), "latitude"]
@@ -30,3 +30,13 @@ quantile(df$Distance)
 usa_center <- as.numeric(geocode("United States"))
 ggmap(get_googlemap(center=usa_center, scale=2, zoom=4), extent="normal") +
  geom_point(data=df, aes(x=Longitude, y=Latitude), color = "red", size = 1, alpha = 0.5)
+
+ggplot(data = df) + 
+  geom_polygon(aes(x = Longitude, y = Latitude, fill = StateFull), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE)
+
+ggplot(data = states) + 
+  geom_polygon(aes(x = long, y = lat, fill = region), color = "white") + 
+  coord_fixed(1.3) +
+  guides(fill=FALSE)
