dbd64bafe27c4e24a420c8c7432cafacc56ac04d
WilliamLi264264 <william.li@princeton.edu>
2018-12-22 23:09:03 -0500

    general changes

--- scripts/team-content-generator.py
+++ scripts/team-content-generator.py
@@ -1,5 +1,7 @@
 import os
-os.system("curl \"https://docs.google.com/spreadsheets/d/1tS_AHvttsNA7WVVQjaR8Wv1kEpn-DmuzI5Ld2A0XPQc/export?gid=0&format=tsv\" > team.tsv")
+os.system(
+    "curl \"https://docs.google.com/spreadsheets/d/1tS_AHvttsNA7WVVQjaR8Wv1kEpn-DmuzI5Ld2A0XPQc/export?gid=0&format=tsv\" > team.tsv"
+)
 
 readFile = open("team.tsv")
 writeFile = open("../team-container-content.html", 'w')
@@ -18,20 +20,25 @@ print >> writeFile, "<h1 class=\"section-title\">Leadership</h1> \n\
     <a href=\"#emeritus\" class=\"table-link\">Emeriti</a> \n\
     <a href=\"#events\" class=\"table-link\">Event Supervisors</a> \n\
   </div> \n"
+
 print >> writeFile, "\t<div id=\"profiles\">"
 
 newSection = True
 printDescription = False
 printPerson = False
-printRow = True                     # True = need to print a new Row
+printRow = True  # True = need to print a new Row
+printEventSupervisors = False
 
 lines = readFile.readlines()
-print len(lines)
-for x in lines:
-    x = x.split('\t')
+numLines = len(lines)
+print numLines
+for i in xrange(numLines):
+    x = lines[i].split('\t')
+    if x[1].strip() == 'events':
+        printEventSupervisors = True
     if x[0].strip() == "":
         if not printRow:
-            print >> writeFile,"\t\t</div>"
+            print >> writeFile, "\t\t</div>"
             printRow = True
         print >> writeFile, ""
         newSection = True
@@ -40,7 +47,8 @@ for x in lines:
         newSection = False
         printDescription = True
         printPerson = True
-        print >> writeFile, "\t\t<h2 class=\"section-subtitle\" id=\"{}\">{} <i class=\"fa fa-angle-up\" id=\"scroll-top\" data-toggle=\"tooltip\" data-placement=\"right\" title=\"Scroll to top\" aria-hidden=\"true\"></i></h2>".format(x[1], x[0])
+        print >> writeFile, "\t\t<h2 class=\"section-subtitle\" id=\"{}\">{} <i class=\"fa fa-angle-up\" id=\"scroll-top\" data-toggle=\"tooltip\" data-placement=\"right\" title=\"Scroll to top\" aria-hidden=\"true\"></i></h2>".format(
+            x[1], x[0])
     elif printDescription:
         printDescription = False
         print >> writeFile, "\t\t<div class=\"col-md-12\"> \n\
@@ -48,27 +56,31 @@ for x in lines:
     </div>".format(x[0])
 
     elif printPerson:
-      hasTitle = False
-      title = ""
-      name = "-".join([temp.strip().lower() for temp in x[0].split(" ")])
-      if x[2] != "":
-        hasTitle = True
-        title = x[2].split(",")
-      if printRow:
-        print >> writeFile, "\t\t<div class=\"row\">"
-      print >> writeFile, "\t\t\t<div class=\"col-md-6\"> \n\
+        if printEventSupervisors:
+            break
+        hasTitle = False
+        title = ""
+        name = "-".join([temp.strip().lower() for temp in x[0].split(" ")])
+        if x[2] != "":
+            hasTitle = True
+            title = x[2].split(",")
+        if printRow:
+            print >> writeFile, "\t\t<div class=\"row\">"
+        print >> writeFile, "\t\t\t<div class=\"col-md-6\"> \n\
           <ul class=\"list-group\"> \n\
             <li class=\"list-group-item\" data-toggle=\"collapse\" href=\"#{}-bio\" aria-expanded=\"false\"> \n\
               <div class=\"profile-main\"> \n\
                 <img src=\"img/bios/{}.jpg\" alt=\"{}\"> \n\
                 <div class=\"description\"> \n\
-                  <div class=\"profile-name\">{} '{}</div>".format(name.strip(), name.strip(), x[0].strip(), x[0].strip(), x[1].strip())
+                  <div class=\"profile-name\">{} '{}</div>".format(
+            name.strip(), name.strip(), x[0].strip(), x[0].strip(),
+            x[1].strip())
 
-      if title != "":
-        for tit in title:
-          print >> writeFile, "\t\t\t\t\t\t\t\t<div class=\"profile-title\">{}</div>".format(tit.strip())
-                
-      print >> writeFile, "\t\t\t\t\t\t\t</div> \n\
+        if title != "":
+            for tit in title:
+                print >> writeFile, "\t\t\t\t\t\t\t\t<div class=\"profile-title\">{}</div>".format(tit.strip())
+
+        print >> writeFile, "\t\t\t\t\t\t\t</div> \n\
               </div> \n\
               <div class=\"collapse profile-bio\" id=\"{}-bio\"> \n\
                 {} \n\
@@ -77,12 +89,41 @@ for x in lines:
           </ul> \n\
         </div>".format(name.strip(), x[3].strip())
 
-      if printRow:
-        printRow = False
-        continue
-      if not printRow:
-        print >> writeFile,"\t\t</div>"
-        printRow = True
+        if printRow:
+            printRow = False
+            continue
+        if not printRow:
+            print >> writeFile, "\t\t</div>"
+            printRow = True
+
+if printEventSupervisors:
+    supervisorCount = 0
+    for i in xrange(i, numLines):
+        x = lines[i].split('\t')
+        name, year, event, school = [x[j].strip() for j in range(4)]
+        filename = "-".join([temp.strip().lower() for temp in name.split(" ")])
+        
+        if supervisorCount % 3 == 0: print >> writeFile, "\t\t<div class=\"row\">"
+        
+        print >> writeFile, '<div class="col-md-4">\n\
+            <ul class="list-group">\n\
+              <li class="list-group-item auto-cursor">\n\
+                <div class="profile-main text-center">\n\
+                  <img src="img/supervisors/{}.jpg" alt="{}">\n\
+                  <div class="description-bottom">\n\
+                    <div class="profile-name">{} \'{}</div>\n\
+                    <div class="profile-title">{}</div>\n\
+                    <div class="profile-title">{}</div>\n\
+                  </div>\n\
+                </div>\n\
+              </li>\n\
+            </ul>\n\
+          </div>'.format(filename, name, name, year, event, school)
+
+        if supervisorCount % 3 == 2: print >> writeFile, "\t\t</div>"
+        supervisorCount += 1
+
+    if supervisorCount % 3 != 2: print >> writeFile, "\t\t</div>"
 
 print >> writeFile, "\t</div>"
 
